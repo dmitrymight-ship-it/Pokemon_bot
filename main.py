f5a81cb53e4d71a1b2389d8a6e7f17251a0be753
dmitrymight-ship-it <dmitrymight@gmail.com>
2026-08-24 13:29:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 import telebot
 from config import TOKEN
-from logic import Pokemon
+from random import randint
+from logic import Pokemon, Wizard, Fighter
 from telebot import types
 
 bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['go'])
-def go(message):
+def start(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
+        chance = randint(1, 3)
+        if chance == 1:
+            pokemon = Pokemon(message.from_user.username)
+        elif chance == 2:
+            pokemon = Wizard(message.from_user.username)
+        elif chance == 3:
+            pokemon = Fighter(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
 
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+        bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
 
-@bot.message_handler(commands=['help'])
+
+@bot.message_handler(commands=['info'])
 def info_(message):
     bot.reply_to(message, """\
 Я универсальный телеграмм бот:
 С моей помощью вы можете создать себе покемона используя функцию /go
-Также вы можете его покормить используя функцию /variant\
+Также вы можете его покормить используя функцию /variant, а с функцией /attack ты сможешь начать бой\
 """)
 
 
@@ -54,4 +74,3 @@ def handle_text(message):
 
 
 bot.infinity_polling(none_stop=True)
-
